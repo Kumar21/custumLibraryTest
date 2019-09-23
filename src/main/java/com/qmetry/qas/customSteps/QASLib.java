319361cc2560ed186b1a6c3c514abfe3ed0711d6
Kumar Shanu <perfectks21@gmail.com>
2019-09-23 14:45:37 +0530

    2nd commit

--- src/main/java/com/qmetry/qas/customSteps/QASLib.java
+++ src/main/java/com/qmetry/qas/customSteps/QASLib.java
@@ -2,9 +2,9 @@ package com.qmetry.qas.customSteps;
 import static com.qmetry.qaf.automation.step.CommonStep.*;
 public class QASLib {
 
-	public static String getUrl(String url) {
-	    System.out.println("Url returned"+ url);
-		return url; 
+	public static void getUrl(String url) {
+		String rr =url;
+	    System.out.println("Url returned"+ rr);
 	}
 }
  
